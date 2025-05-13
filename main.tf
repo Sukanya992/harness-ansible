5d710d0c010e86b4f73a9d28f8bb14eb5cac44f7
Sukanya992 <sukanya09200206@gmail.com>
2025-05-13 10:20:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "ansible-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
+  }
+}
